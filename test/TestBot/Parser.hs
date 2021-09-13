86b6829ac3a52341e13ad86a9c7dfc009d1537c4
LyuPo <lport@frtk.ru>
2021-09-13 16:10:13 +0300

    Add tests to Bot.Tele.Run

--- test/TestBot/Parser.hs
+++ test/TestBot/Parser.hs
@@ -12,6 +12,7 @@ import qualified Bot.Tele.Parser.ParserSpec as ParserSpec
 import qualified Bot.Tele.Parser.Data as DParser
 
 import Test.Tasty.Hspec
+import Test.Hspec()
 
 spec_parseUpdateData :: B.ByteString -> B.ByteString -> B.ByteString -> Spec
 spec_parseUpdateData obj bstr bstrFail = describe "Testing update parse for telegram bot" $ do
